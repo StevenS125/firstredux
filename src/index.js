a22924e593019f87c6a962a406df6487726185e4
StevenS125 <sts12589@gmail.com>
2018-01-24 17:59:35 -0500

    starting webpack server

--- src/index.js
+++ src/index.js
@@ -1,15 +1,47 @@
 import C from './constants'
-import { allSkiDays, goal } from './initialState.json'
+import appReducer from './store/reducers'
+import initialState from './initialState.json'
+
+let state = initialState
 
 console.log(`
 
-   Ski Day Counter
-   ================
-   The goal is ${goal} days 
-   Initially there are ${allSkiDays.length} ski days in state 
+	Initial state
+	=============
+	goal: ${state.goal}
+	resorts: ${JSON.stringify(state.allSkiDays)}
+	fetching: ${state.resortNames.fetching}
+	suggestions: ${state.resortNames.suggestions}
 
-   Constants (actions)
-   -------------------
-   ${Object.keys(C).join('\n     ')}
+`)
+
+state = appReducer(state, {
+	type: C.SET_GOAL,
+	payload: 2
+})
+
+state = appReducer(state, {
+	type: C.ADD_DAY,
+	payload: {
+		"resort": "Mt Shasta",
+		"date": "2016-10-28",
+		"powder": false,
+		"backcountry": true
+	}
+})
+
+state = appReducer(state, {
+	type: C.CHANGE_SUGGESTIONS,
+	payload: ["Mt Tallac", "Mt Hood", "Mt Shasta"]
+})
+
+console.log(`
+
+	Next state
+	=============
+	goal: ${state.goal}
+	resorts: ${JSON.stringify(state.allSkiDays)}
+	fetching: ${state.resortNames.fetching}
+	suggestions: ${state.resortNames.suggestions}
 
 `)
